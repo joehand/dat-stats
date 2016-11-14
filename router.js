5df9c1aec1d28a00fab5fff650ff7d33d32aea8a
Joe Hand <joe@joeahand.com>
2016-11-14 10:44:06 -0800

    upgrade packages

--- router.js
+++ router.js
@@ -1,25 +1,16 @@
 const serverRouter = require('server-router')
-const browserify = require('browserify')
-const bankai = require('bankai')()
+const bankai = require('bankai')
 const hyperServer = require('hypercore-stats-server')
 
 module.exports = function (archive) {
-  const router = serverRouter('/404')
+  const assets = bankai(require.resolve('./index.js'))
 
-  const html = bankai.html({})
-  router.on('/', (req, res) => html(req, res).pipe(res))
-
-  const css = bankai.css()
-  router.on('/bundle.css', (req, res) => css(req, res).pipe(res))
-  const js = bankai.js(browserify, require.resolve('./index.js'))
-  router.on('/bundle.js', (req, res) => js(req, res).pipe(res))
-
-  router.on('/events', (req, res) => hyperServer(archive, res))
-
-  router.on('/404', (req, res) => {
-    res.statusCode = 404
-    res.end('{ "message": "the server is confused" }')
-  })
+  const router = serverRouter([
+    ['/', (req, res) => assets.html(req, res).pipe(res)],
+    ['/bundle.css', (req, res) => assets.css(req, res).pipe(res)],
+    ['/bundle.js', (req, res) => assets.js(req, res).pipe(res)],
+    ['/events', (req, res) => hyperServer(archive, res)]
+  ])
 
   return router
 }
