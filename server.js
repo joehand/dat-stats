7dd1e6d2aca59e907d8e12386c7a99c3adcc1c22
Joe Hand <joe@joeahand.com>
2016-11-14 11:02:09 -0800

    add args to server

--- server.js
+++ server.js
@@ -3,14 +3,30 @@ const hyperdrive = require('hyperdrive')
 const serve = require('hyperdrive-http')
 const swarm = require('hyperdrive-archive-swarm')
 const memdb = require('memdb')
+const minimist = require('minimist')
 const stats = require('./router')
+
+const argv = minimist(process.argv.slice(2), {
+  default: {
+    statsPort: 10000,
+    drivePort: 8000
+  }
+})
+
+if (!argv._[0]) throw new Error('link required')
+const link = argv._[0]
+
+const statsPort = argv.statsPort
+const drivePort = argv.drivePort
 
 const drive = hyperdrive(memdb())
 
 // a remote dat
-const archive = drive.createArchive('cc2f0ca624b44a962c15bd24ca07ab3785df618d7a65f27059bebcc004eb6ff8')
+const archive = drive.createArchive(link)
 
-http.createServer(stats(archive)).listen(10000) // stats ui hosted at http://localhost:10000
-http.createServer(serve(archive)).listen(8000) // file hosted at http://localhost:8000/sintel.mp4
-
+http.createServer(stats(archive)).listen(statsPort)
+http.createServer(serve(archive)).listen(drivePort)
 swarm(archive)
+
+console.log(`Serving stats at http://localhost:${statsPort}/`)
+console.log(`Serving files at http://localhost:${drivePort}/`)
